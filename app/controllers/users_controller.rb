1a583f1fe53b05afff3b0ed7f146f5bd277a5270
Kevin J <7211424+Xmaxer@users.noreply.github.com>
2018-10-06 13:42:13 +0100

    Added migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
   def new
+    if !logged_in? && User.count >= 1
+      render 'static_pages/error'
+  else
     @user = User.new
+  end
   end
   def create
     @user = User.new(user_params)
     if @user.save
+      if !logged_in?
+        login @user
+      end
       flash[:success] = "New user added successfully"
       redirect_to dashboard_url
     else
