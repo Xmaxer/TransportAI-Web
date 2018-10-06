6c39f4689f784e62c50e60699c46bad88b69c32d
Kevin J <7211424+Xmaxer@users.noreply.github.com>
2018-10-06 13:02:51 +0100

    Implemented heroku + Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "New user added successfully"
+      redirect_to dashboard_url
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get '/new_admin', to: 'users#new'
   get '/dashboard', to: 'dashboard#index'
   post '/login', to: 'sessions#create'
-  root 'static_pages#home'
+  post '/new_admin', to: 'users#create'
+  root 'static_pages#login'
   get '/login', to: 'static_pages#login'
   get 'error', to: 'static_pages#error'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
