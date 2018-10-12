1b2ebcaaf68fd7bfdd4bc4d8f3a2c20175404b75
MrBobaloo <33062819+MrBobaloo@users.noreply.github.com>
2018-10-12 11:09:30 +0100

    Add files via upload
    changed tags to classes in css. Added rounded corners on form for login. Added LoginError function to notify if a login failed. Fixed text change to notify if a login failed.

---
+++ js/LoginError.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.btn_login').click(function(){
+		console.log("Hello");
+		var button = $(this);
+
+			var title = $('.change_text');
+			title.text("Login Failed");
+	});
+});
+
+	
+    
