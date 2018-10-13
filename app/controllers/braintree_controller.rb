7a584b37bdceb737409300a73b1818b5540ac68f
Kevin J <7211424+Xmaxer@users.noreply.github.com>
2018-10-13 19:33:07 +0100

    Removed requirement for customer ID in get request

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -6,13 +6,9 @@ class BraintreeController < ApplicationController
       public_key: "42gn5r2h5vkmd2fr",
       private_key: "88c96dfe47fc2633d083f8edb42dbff5"
     )
-    if params[:customer_id]
-      @client_token = @gateway.client_token.generate(
-        customer_id: params[:customer_id]
-      )
-    else
-      render 'braintree/invalid_customer_id'
-    end
+    @client_token = @gateway.client_token.generate(
+      customer_id: params[:customer_id]
+    )
   end
   def checkout
     nonce_from_the_client = params[:payment_method_nonce]
