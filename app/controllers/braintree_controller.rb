00c6ec7db6cc02f854e7115953500b3250542690
Kevin J <7211424+Xmaxer@users.noreply.github.com>
2018-10-13 19:25:45 +0100

    Added transaction creation

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -1,16 +1,25 @@
 class BraintreeController < ApplicationController
   def client_token
-    gateway = Braintree::Gateway.new(
+    @gateway = Braintree::Gateway.new(
       environment: :sandbox,
       merchant_id: "s2w84n9y8wd4dkpm",
       public_key: "42gn5r2h5vkmd2fr",
       private_key: "88c96dfe47fc2633d083f8edb42dbff5"
     )
-    @client_token = gateway.client_token.generate(
-      customer_id: params[:customer_id]
-    )
+    if params[:customer_id]
+      @client_token = @gateway.client_token.generate(
+        customer_id: params[:customer_id]
+      )
+    else
+      render 'braintree/invalid_customer_id'
+    end
   end
   def checkout
-    
+    nonce_from_the_client = params[:payment_method_nonce]
+
+    result = @gateway.transaction.sale(
+      amount: params[:amount],
+      payment_method_nonce: nonce_from_the_client
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,8 @@ Rails.application.routes.draw do
   get '/login', to: 'static_pages#login'
   get 'error', to: 'static_pages#error'
   delete '/logout',  to: 'sessions#destroy'
-  get '/braintree/client_token' do
-    gateway.client_token.generate
-  end
-  post 'braintree/checkout' do
-    nonce_from_the_client = params[:payment_method_nonce]
-  end
+  get 'braintree/client_token'
+  post 'braintree/checkout'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
