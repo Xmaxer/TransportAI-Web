78fa1b31fc7e366bdf01ac1c9ca640c8fdb50682
Kevin J <7211424+Xmaxer@users.noreply.github.com>
2018-10-13 20:45:51 +0100

    Fixed POST response

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -1,12 +1,16 @@
 class BraintreeController < ApplicationController
   layout false
-  def client_token
+  skip_before_action :verify_authenticity_token
+  before_action do
     @gateway = Braintree::Gateway.new(
       environment: :sandbox,
       merchant_id: "s2w84n9y8wd4dkpm",
       public_key: "42gn5r2h5vkmd2fr",
       private_key: "88c96dfe47fc2633d083f8edb42dbff5"
     )
+  end
+
+  def client_token
     @client_token = @gateway.client_token.generate(
       customer_id: params[:customer_id]
     )
@@ -18,5 +22,10 @@ class BraintreeController < ApplicationController
       amount: params[:amount],
       payment_method_nonce: nonce_from_the_client
     )
+
+    respond_to do |format|
+      msg = result
+      format.json {render :json => msg}
+    end
   end
 end
